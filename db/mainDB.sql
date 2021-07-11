8ed3a76855ef0bbaad8d001407dd99b0153940ab
Naushikha <naushikha.jayawickrama@gmail.com>
2021-07-11 16:52:46 +0530

    Many improvements to API

--- db/mainDB.sql
+++ db/mainDB.sql
@@ -1,31 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 5.0.4
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Generation Time: Jul 08, 2021 at 04:17 PM
--- Server version: 10.4.16-MariaDB
--- PHP Version: 7.4.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `VO`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user`
---
 
 CREATE TABLE `user` (
   `id` int(11) PRIMARY KEY,
@@ -39,17 +12,25 @@ CREATE TABLE `user` (
   `address` varchar(400)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `user`
---
-
 INSERT INTO `user` (`id`, `first_name`, `last_name`, `email`, `contact_number`, `password`, `dob`, `gender`, `address`) VALUES
-(0, 'Admin', '', 'admin@vo.com', '0000000000', '123', '2021-09-09', 'A', 'VirtualOffice'),
-(1, 'Naushikha', 'Jayawickrama', 'naushikha@outlook.com', '0763316991', '123', '1997-09-09', 'M', 'School Lane, Kadawatha.'),
-(2, 'Imashi', 'Dissanayake', 'imashi921a@gmail.com', '0703081775', '123', '1997-09-21', 'F', 'Armpitiya, Kandy.'),
-(3, 'mamaaaa', 'Oooooooohohohh', 'stuff', '01234', 'pass', '2020-11-01', 'M', '19');
-COMMIT;
+(0, 'Admin', '', 'admin@vo', '0000000000', '3c9909afec25354d551dae21590bb26e38d53f2173b8d3dc3eee4c047e7ab1c1eb8b85103e3be7ba613b31bb5c9c36214dc9f14a42fd7a2fdb84856bca5c44c2', '2021-09-09', 'A', 'VirtualOffice'),
+(1, 'Naushikha', 'Jayawickrama', 'naushikha@outlook.com', '0763316991', '3c9909afec25354d551dae21590bb26e38d53f2173b8d3dc3eee4c047e7ab1c1eb8b85103e3be7ba613b31bb5c9c36214dc9f14a42fd7a2fdb84856bca5c44c2', '1997-09-09', 'M', 'School Lane, Kadawatha.'),
+(2, 'Imashi', 'Dissanayake', 'imashi921a@gmail.com', '0703081775', '3c9909afec25354d551dae21590bb26e38d53f2173b8d3dc3eee4c047e7ab1c1eb8b85103e3be7ba613b31bb5c9c36214dc9f14a42fd7a2fdb84856bca5c44c2', '1997-09-21', 'F', 'Armpitiya, Kandy.'),
+(3, 'John', 'Doe', 'john.doe@gmail.com', '0123456789', '3c9909afec25354d551dae21590bb26e38d53f2173b8d3dc3eee4c047e7ab1c1eb8b85103e3be7ba613b31bb5c9c36214dc9f14a42fd7a2fdb84856bca5c44c2', '2020-11-01', 'M', 'Sri Lanka');
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+CREATE TABLE `vo_settings` (
+  `id` int(11) PRIMARY KEY,
+  `option` varchar(255),
+  `value` varchar(255)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO `vo_settings` (`id`, `option`, `value`) VALUES
+(0, 'org_setup','not-done'),
+(1, 'org_name','VirtualOffice'),
+(2, 'org_country','Sri Lanka'),
+(3, 'email_host',''),
+(4, 'email_port',''),
+(5, 'email_address',''),
+(6, 'email_password','');
+
+COMMIT;
--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const serverAddress = "localhost";
-const serverPort = 3000;
+const serverPort = 3030;
 
 const DBHost = "localhost";
 const DBUser = "VO";
 const DBPassword = "123";
 const DBDatabase = "VO";
+var DBInitialSetup = 0;
+
+const jwtKey = "keykeykey";
+const apiVersion = "v1";
 
 const express = require("express");
+var cors = require("cors");
 const app = express();
 const mysql = require("mysql");
 const jwt = require("jsonwebtoken");
@@ -20,8 +25,31 @@ const db = mysql.createConnection({
 });
 
 db.connect(function (err) {
-  if (err) throw err;
+  if (err) {
+    console.log(
+      "Error connecting to VirtualOffice DB. Maybe MySQL isn't running?"
+    );
+    // throw err;
+    process.exit();
+  }
   console.log("Connected with VirtualOffice DB.");
+  // Check if VO DB needs initial setup
+  db.query(
+    "SELECT value FROM vo_settings WHERE option = 'org_setup'",
+    (error, results, fields) => {
+      if (error) throw error;
+      if (results.length) {
+        if (results[0].value == "done") {
+          DBInitialSetup = 1; // The initial setup has been done
+        } else {
+          DBInitialSetup = 0;
+          console.log(
+            "VirtualOffice DB requires an initial setup by the administrator."
+          );
+        }
+      }
+    }
+  );
 });
 
 app.use(express.json()); // to support JSON-encoded bodies
@@ -31,6 +59,7 @@ app.use(
     extended: true,
   })
 );
+app.use(cors());
 
 var server = app.listen(serverPort, serverAddress, function () {
   var host = server.address().address;
@@ -39,75 +68,132 @@ var server = app.listen(serverPort, serverAddress, function () {
 });
 
 // Welcome message for root
-app.get("/api/", function (req, res) {
+app.get(`/api/${apiVersion}/`, function (req, res) {
   res.json({ success: "You've reached VirtualOffice API." });
 });
 
 // General
 // Get all users
-app.get("/api/user", function (req, res) {
+app.get(`/api/${apiVersion}/user`, function (req, res) {
   db.query("SELECT * FROM user", function (error, results, fields) {
     if (error) throw error;
     res.end(JSON.stringify(results));
   });
 });
 // Login
-app.post("/api/login", (req, res) => {
-  const user = {
-    id: 1,
-    email: req.body.email,
-    expireTime: 1000,
-  };
-  jwt.sign({ user: user }, "secretkey", (err, token) => {
-    res.json({ token });
-  });
+app.post(`/api/${apiVersion}/login`, (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const keepLogged = req.body.keepLogged; // Increase expire time if this is enabled
+  // Hash the password
+  const crypto = require("crypto");
+  const hashedPassword = crypto
+    .createHash("sha512")
+    .update(password)
+    .digest("hex");
+  // TO DO: Check for validation
+  // Check if user with email and password exist
+  db.query(
+    "SELECT id, first_name, last_name FROM user WHERE email = ? AND password = ?",
+    [email, hashedPassword],
+    (error, results, fields) => {
+      if (error) throw error;
+      if (results.length) {
+        let info;
+        // Indicate frontend that an initial setup is required
+        if (!DBInitialSetup) {
+          // First check if this is the admin
+          // Admin user id is 0
+          if (results[0].id == "0") {
+            // console.log(
+            //   `Admin user logged in @ ${new Date().toLocaleString()}`
+            // );
+            info = { initialSetup: true };
+          } else {
+            res.json({
+              error:
+                "VirtualOffice DB is not setup yet. Contact administrator.",
+            });
+          }
+        }
+        const user = {
+          id: results[0].id,
+          email: results[0].email,
+          first_name: results[0].first_name,
+          last_name: results[0].last_name,
+          expire: 7200, // Logged in for 2 hours
+        };
+        jwt.sign({ user: user }, jwtKey, (err, token) => {
+          res.json({ token, info }); // Just send back the token
+        });
+      } else {
+        res.json({ error: "Login failed." });
+      }
+    }
+  );
+  return;
 });
 // Logout
-app.post("/api/logout", verifyToken, (req, res) => {
-  jwt.verify(req.token, "secretkey", (err, authData) => {
-    if (err) {
-      res.sendStatus(403);
-    } else {
-      res.json({
-        success: "Logged out",
-        authData,
-      });
-    }
-  });
+app.post(`/api/${apiVersion}/logout`, verifyJWT, (req, res) => {
+  res.json(req.authData);
 });
 // Initial setting up
-app.post("/initial-setup", function (req, res) {
-  res.end("initial setting up");
+app.post(`/api/${apiVersion}/initial-setup`, function (req, res) {
+  res.end("initial user set up");
 });
 
-function verifyToken(req, res, next) {
+// This can be used to verify login status
+function verifyJWT(req, res, next) {
   const bearerHeader = req.headers["authorization"];
   if (typeof bearerHeader !== "undefined") {
     const bearerToken = bearerHeader.split(" ")[1];
-    req.token = bearerToken;
+    // req.token = bearerToken;
+    jwt.verify(bearerToken, jwtKey, (err, authData) => {
+      if (err) {
+        // Token is bad
+        res.status(403).json({
+          error: "You are not authenticated.",
+        });
+      } else {
+        // Token is good
+        req.authData = authData; // Store authData
+      }
+    });
     next();
   } else {
-    res.sendStatus(403);
+    res.status(403).json({
+      error: "You are not authenticated.",
+    });
   }
 }
 
 // Admin routes
 // https://www.restapitutorial.com/lessons/httpmethods.html
 
-app.put("/api/admin/initial-setup", function (req, res) {
-  res.end("initial setting up");
-});
+app.post(
+  `/api/${apiVersion}/admin/initial-setup`,
+  verifyJWT,
+  function (req, res) {
+    res.end("initial setting up");
+  }
+);
 // Get all existing users
-app.get("/api/admin/user", function (req, res) {
+app.get(`/api/${apiVersion}/admin/users`, verifyJWT, function (req, res) {
+  // First check if the logged in user is an admin
+  if (req.authData.user.id != "0") {
+    res.status(403).json({
+      error: "You are not authorized to access this resource.",
+    });
+  }
   db.query("SELECT * FROM user", function (error, results, fields) {
     if (error) throw error;
     res.json(results);
   });
 });
-app.post("/api/admin/user", function (req, res) {
+app.post(`/api/${apiVersion}/admin/user`, function (req, res) {
   res.end("Add new user, email+name");
 });
-app.delete("/api/admin/user/:id", function (req, res) {
+app.delete(`/api/${apiVersion}/admin/user/:id`, function (req, res) {
   db.query(
     "DELETE FROM user WHERE id=?",
     [req.params.id],
@@ -119,8 +205,8 @@ app.delete("/api/admin/user/:id", function (req, res) {
   res.end("Remove user");
 });
 
-// Helpful to rest entire thing to start from scratch
-app.get("/api/admin/reset-db", async function (req, res) {
+// Helpful to reset entire thing to start from scratch
+app.get(`/api/${apiVersion}/admin/reset-db`, async function (req, res) {
   console.log("Resetting database...");
   db.query(
     `SELECT CONCAT('DROP TABLE IF EXISTS ${"`"}', table_name, '${"`"};')
